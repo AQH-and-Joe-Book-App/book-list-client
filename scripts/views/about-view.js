e2b6c73064ef482e822c29d67f4796ddf1f0154a
Joe Waine <joe.waine@gmail.com>
2017-11-08 09:29:40 -0800

    adding styling

---
+++ scripts/views/about-view.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+  const aboutView = {};
+
+  aboutView.initAboutPage = function() {
+
+
+
+
+  }
+
+  module.aboutView = aboutView;
+})(app)
+
+
+$(function() {
+// alert('hello');
+})
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -6,8 +6,10 @@ var app = app || {};
   const bookView = {};
 
   bookView.initIndexPage = function() {
+
     $('.container').hide();
     $('.book-view').show();
+    $('.book-count').text(app.Book.all.length + ' books available');
     app.Book.all.map(book => $('#book-list').append(book.toHtml()));
   }
 
