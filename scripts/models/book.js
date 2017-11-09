391f9f8a78017d65396d3b715285e8a0b65abf79
Joe Waine <joe.waine@gmail.com>
2017-11-09 13:37:03 -0800

    middle ware lab

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var app = app || {};
-var __API_URL__ = 'https://aqh-jw-booklist.herokuapp.com';
+// var __API_URL__ = 'https://aqh-jw-booklist.herokuapp.com';
+var __API_URL__ = 'http://localhost:3000';
 
 (function(module) {
+
+
   function errorCallback(err) {
     console.error(err);
     module.errorView.initErrorPage(err);
@@ -30,17 +33,42 @@ var __API_URL__ = 'https://aqh-jw-booklist.herokuapp.com';
       .then(callback)
       .catch(errorCallback);
 
-  Book.fetchOne = (ctx,callback)  =>
-    $.get(`${__API_URL__}/api/v1/books/${ctx.params.book_id}`)
-      .then(results => ctx.book = results[0])
+  Book.fetchOne = (ctx, callback)  =>
+      $.get(`${__API_URL__}/api/v1/books/${ctx.params.book_id}`)
+      .then(results => ctx.book = results[0]
+      )
       .then(callback)
       .catch(errorCallback);
 
-Book.create = book =>
+Book.deleteOne = (ctx, callback)  =>
+          $.ajax({
+              url: `${__API_URL__}/api/v1/books/${ctx.params.book_id}`, // your api url
+              method: 'DELETE', // method is any HTTP method
+              success: function() {
+                page('/');
+                callback();
+              }
+          });
+
+
+  // ask about this
+  Book.update = (book) =>
+      $.ajax({
+          url: `${__API_URL__}/api/v1/books/${book.book_id}`, // your api url
+          method: 'PUT', // method is any HTTP method
+          data: book, // data as js object
+          success: function() {
+            page('/');
+          }
+      });
+
+
+Book.create = (book, callback) =>
   $.post(`${__API_URL__}/api/v1/books`, book)
     .then(() => page('/'))
+    .then(callback)
     .catch(errorCallback);
-
   module.Book = Book;
+
 
 })(app)
