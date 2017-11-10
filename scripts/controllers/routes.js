22030c0b364be4a2992e0ce8be0628c02a40e250
Joe Waine <joe.waine@gmail.com>
2017-11-10 13:39:07 -0800

    friday changes

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -7,6 +7,4 @@ page('/books/:book_id/delete', ctx => app.Book.deleteOne(ctx, app.bookView.initI
 page('/admin', (ctx, next) => app.adminView.verify(ctx,next), ctx => app.adminView.initAdminPage(ctx));
 page('/books/:book_id/update', (ctx, next) => app.adminView.verify(ctx,next), (ctx, next) => app.adminView.initAdminPage(ctx, next), (ctx) => app.Book.fetchOne(ctx, app.bookView.initUpdateFormPage(ctx)));
 
-
-
 page();
